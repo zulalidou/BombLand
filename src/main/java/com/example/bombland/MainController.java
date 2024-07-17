ae52a5dd4924bc7c27e388befa2dcdb00283307b
Zul <zulkifl.alidou@gmail.com>
2024-07-17 15:20:40 -0700

    feat: implement EXIT button feature on main menu

--- src/main/java/com/example/bombland/MainController.java
+++ src/main/java/com/example/bombland/MainController.java
@@ -1,8 +1,11 @@
 package com.example.bombland;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class MainController {
-
+    @FXML
+    private void closeApp() {
+        Platform.exit();
+    }
 }
