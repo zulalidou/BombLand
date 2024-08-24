83f4e126bc968e7bb6154fe23fbd6ad9dee46ccb
Zul <zulkifl.alidou@gmail.com>
2024-08-24 16:07:28 -0700

    style: change appearence of instructions page

--- src/main/java/com/example/bombland/InstructionsController.java
+++ src/main/java/com/example/bombland/InstructionsController.java
@@ -3,17 +3,36 @@ package com.example.bombland;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import java.io.IOException;
 
 public class InstructionsController {
     @FXML
-    Button backBtn;
+    VBox instructionsPage, instructionsContainer;
+
+    @FXML
+    HBox instructionsContainer_leftChild, instructionsContainer_middleChild, instructionsContainer_rightChild;
+
+    @FXML
+    ScrollPane instructionsScrollPane;
+
+    @FXML
+    public void initialize() {
+        HBox.setHgrow(instructionsContainer_leftChild, Priority.ALWAYS);
+        HBox.setHgrow(instructionsContainer_middleChild, Priority.ALWAYS);
+        HBox.setHgrow(instructionsContainer_rightChild, Priority.ALWAYS);
+
+        // Makes sure the child of the scrollpane has the same width as the scrollpane
+        instructionsScrollPane.setFitToWidth(true);
+        VBox.setVgrow(instructionsScrollPane, Priority.ALWAYS);
+    }
 
     @FXML
     private void goToMainMenu(ActionEvent event) throws IOException {
-        ScreenController screenController = new ScreenController(backBtn.getScene());
+        ScreenController screenController = new ScreenController(instructionsPage.getScene());
         screenController.addScreen("main", FXMLLoader.load(getClass().getResource("/com/example/bombland/FXML/main-view.fxml")));
         screenController.activate("main");
     }
