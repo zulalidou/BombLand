0827e75c3c51d0efd87b579bf1049f049738bd6a
Zul <zulkifl.alidou@gmail.com>
2024-09-02 15:15:49 -0700

    style: improve appearence of mode-selection page

--- src/main/java/com/example/bombland/ModeSelectionController.java
+++ src/main/java/com/example/bombland/ModeSelectionController.java
@@ -1,18 +1,23 @@
 package com.example.bombland;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-
 import java.io.IOException;
 
 public class ModeSelectionController {
     @FXML
-    VBox modeSelectionPage;
+    VBox modeSelectionPage, modeSelectionOptions_outerContainer;
 
     @FXML
-    private void goToMainMenu(ActionEvent event) throws IOException {
+    public void initialize() {
+        // Prevents the width of the modeSelectionOptions_outerContainer VBox from having the same width as its parent container (modeSelectionPage)
+        modeSelectionOptions_outerContainer.setFillWidth(false);
+    }
+
+    @FXML
+    private void goToMainMenu() throws IOException {
         ScreenController screenController = new ScreenController(modeSelectionPage.getScene());
         screenController.addScreen("main", FXMLLoader.load(getClass().getResource("/com/example/bombland/FXML/main-view.fxml")));
         screenController.activate("main");
